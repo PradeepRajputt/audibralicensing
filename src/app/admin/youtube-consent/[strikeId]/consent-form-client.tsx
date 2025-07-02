38c30a9825b540b92aa7f39f86d4ca38509fba23
Guddu Mishra <onlinewlallah99@gmail.com>
2025-07-02 10:03:43 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/admin/youtube-consent/[strikeId]/consent-form-client.tsx
+++ src/app/admin/youtube-consent/[strikeId]/consent-form-client.tsx
@@ -6,7 +6,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle, CardFooter } from "@/components/ui/card";
 import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Checkbox } from "@/components/ui/checkbox";
@@ -144,4 +144,3 @@ export default function ConsentFormClient({ report, creator }: { report: Report,
     </div>
   );
 }
-
